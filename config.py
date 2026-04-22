47b20af2c7f86a250f620126a77d30a0ecd807d7
alimohm <69640611+alimohm@users.noreply.github.com>
2026-04-22 14:07:19 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,16 @@
+import os
 
+class Config:
+    # سر الأمان للتطبيق
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'mahjoub_secret_key_2026')
+    
+    # رابط قاعدة البيانات (سحبه من المتغيرات التي وضعناها في Railway)
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
+    # إعدادات منصة قمرة
+    QUMRA_API_KEY = os.environ.get('QUMRA_API_KEY')
+    QUMRA_API_URL = os.environ.get('QUMRA_API_URL')
+
+    # إعدادات لغة التطبيق والتوقيت
+    JSON_AS_ASCII = False
