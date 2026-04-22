a1e37c8230ac4e68f0dbdf7afc82bfbf0bd5dbbf
alimohm <69640611+alimohm@users.noreply.github.com>
2026-04-22 14:40:30 +0300

    Update routes.py

--- supplier_panel/routes.py
+++ supplier_panel/routes.py
@@ -1,10 +1,10 @@
 from flask import Blueprint, render_template
 
-# تعريف بوابة الموردين
 supplier_bp = Blueprint('supplier', __name__, 
                         template_folder='templates',
                         static_folder='static')
 
 @supplier_bp.route('/supplier/login')
 def login():
-    return "<h1>صفحة دخول الموردين - قريباً</h1>"
+    # تأكد من وجود المجلد supplier داخل templates الموردين
+    return "<h1>بوابة الموردين قيد التجهيز</h1>"
