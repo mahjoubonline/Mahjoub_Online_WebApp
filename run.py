13cfb271538619fe0448185924c82b367be7ac25
alimohm <69640611+alimohm@users.noreply.github.com>
2026-04-23 17:07:09 +0300

    Update run.py

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ app = create_app()
 
 if __name__ == '__main__':
     # الحصول على المنفذ (Port) من نظام التشغيل
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     
     # تشغيل السيرفر
     app.run(host='0.0.0.0', port=port, debug=False)
