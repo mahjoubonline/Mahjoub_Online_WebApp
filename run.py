770b2b60641162b2b6b20e32b468c727d82dcba4
alimohm <69640611+alimohm@users.noreply.github.com>
2026-04-23 17:02:05 +0300

    Update run.py

--- run.py
+++ run.py
@@ -5,14 +5,8 @@ from core import create_app, db
 app = create_app()
 
 if __name__ == '__main__':
-    with app.app_context():
-        try:
-            # اختبار الاتصال برابط Render الذي وضعته في Variables
-            db.engine.connect()
-            print("--- ✅ Connection to Render Success! ---")
-        except Exception as e:
-            print(f"--- ❌ Connection Failed: {str(e)} ---")
-
-    # تحديد المنفذ تلقائياً لـ Railway
+    # الحصول على المنفذ (Port) من نظام التشغيل
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    
+    # تشغيل السيرفر
+    app.run(host='0.0.0.0', port=port, debug=False)
