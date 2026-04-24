28ae5a30d7f543da004d0cb6218a9628da6ece75
alimohm <69640611+alimohm@users.noreply.github.com>
2026-04-24 22:23:54 +0300

    Update __init__.py

--- supplier_panel/__init__.py
+++ supplier_panel/__init__.py
@@ -1,12 +1,6 @@
 from flask import Blueprint
 
-# تعريف البلوبرنت الخاص بالموردين
-# سنستخدم اسم 'supplier_panel' لكي يتوافق مع استدعاءات url_for
-supplier_bp = Blueprint(
-    'supplier_panel', 
-    __name__, 
-    template_folder='templates' # يخبر Flask بالبحث عن HTML داخل مجلد templates الخاص بالموردين
-)
+# إضافة template_folder لضمان توجه النظام للمجلد الصحيح
+supplier_bp = Blueprint('supplier_panel', __name__, template_folder='templates')
 
-# استيراد المسارات (Routes) في الأسفل لتجنب مشكلة الاستيراد الدائري (Circular Import)
 from . import routes
