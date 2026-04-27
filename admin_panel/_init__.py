ab481ba3b2513b5ede6ec7ead3feab7ab8d38e1f
alimohm <69640611+alimohm@users.noreply.github.com>
2026-04-27 16:15:57 +0300

    Update _init__.py

--- admin_panel/_init__.py
+++ admin_panel/_init__.py
@@ -7,4 +7,15 @@ admin_bp = Blueprint(
     static_folder='static'
 )
 
+from . import routesfrom flask import Blueprint
+
+# تعريف البوابة السيادية للإدارة
+admin_bp = Blueprint(
+    'admin_panel', 
+    __name__, 
+    template_folder='templates',
+    static_folder='static'
+)
+
+# استيراد المسارات لربطها بالبوابة بعد تعريف admin_bp
 from . import routes
