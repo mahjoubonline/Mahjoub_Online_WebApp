0ff330032f052fd14347d9228a8a3a72a31ceb48
alimohm <69640611+alimohm@users.noreply.github.com>
2026-04-27 15:43:15 +0300

    Update _init__.py

--- admin_panel/_init__.py
+++ admin_panel/_init__.py
@@ -3,7 +3,8 @@ from flask import Blueprint
 admin_bp = Blueprint(
     'admin_panel', 
     __name__, 
-    template_folder='templates'
+    template_folder='templates',
+    static_folder='static'
 )
 
 from . import routes
