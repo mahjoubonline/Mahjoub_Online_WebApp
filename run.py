2b669afbcd7f4c7e2de7c4be9d57646b249ff8f5
alimohm <69640611+alimohm@users.noreply.github.com>
2026-04-27 14:30:08 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,9 +1,15 @@
 import os
+import sys
+
+# إجبار بايثون على إضافة المجلد الحالي للمسار
+path = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, path)
+
 from core import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
-    # الحصول على البورت من Railway أو استخدام 5000 كافتراضي
+    # هذا السطر يضمن أن Railway سيجد البورت الصحيح
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
