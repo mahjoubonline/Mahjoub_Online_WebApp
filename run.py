cfaf4d1b2c687b92dd7893e471c3f5594eae8b0f
alimohm <69640611+alimohm@users.noreply.github.com>
2026-04-27 14:30:33 +0300

    Update run.py

--- run.py
+++ run.py
@@ -11,5 +11,5 @@ app = create_app()
 
 if __name__ == "__main__":
     # هذا السطر يضمن أن Railway سيجد البورت الصحيح
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port)
