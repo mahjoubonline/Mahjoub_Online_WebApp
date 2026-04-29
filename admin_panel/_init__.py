a47b46d4b564cd57a38c20526b5fb4d7d83be2e7
alimohm <69640611+alimohm@users.noreply.github.com>
2026-04-29 16:12:53 +0300

    Update _init__.py

--- admin_panel/_init__.py
+++ admin_panel/_init__.py
@@ -1,19 +1,1 @@
-from flask import Blueprint, render_template, redirect, url_for, flash, request
-from flask_login import login_user, logout_user, login_required, current_user
-from core.models.user import User
 
-# تعريف البلوبرينت وتحديد مكان القوالب بدقة
-admin_bp = Blueprint('admin_panel', __name__, template_folder='templates')
-
-@admin_bp.route('/login', methods=['GET', 'POST'])
-def admin_login():
-    if request.method == 'POST':
-        # منطق التحقق من الهوية هنا
-        pass
-    # البحث سيتم داخل admin_panel/templates/admin_panel/login.html
-    return render_template('admin_panel/login.html')
-
-@admin_bp.route('/dashboard')
-@login_required
-def admin_dashboard():
-    return render_template('admin_panel/dashboard.html')
