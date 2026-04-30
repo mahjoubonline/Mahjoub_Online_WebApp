154fddcd71988f33f51ad9770b3c4b384bd58f4d
alimohm <69640611+alimohm@users.noreply.github.com>
2026-04-30 21:05:32 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,13 @@
 import os
 
 class Config:
-    # الرابط الجديد الذي زودتني به لربط الترسانة بقاعدة Render
-    SQLALCHEMY_DATABASE_URI = 'postgresql://mahjoub_online_1_db_user:S7dxtVGcKwrsM1QEzGOuPPcRL8dKxgXk@dpg-d79tuthr0fns73epej4g-a.oregon-postgres.render.com/mahjoub_online_1_db'
+    # جلب الرابط من البيئة (Render) أو استخدام الرابط المباشر الذي قدمته
+    # ملاحظة: Render أحياناً يرسل الرابط بـ postgres:// ويجب تحويله لـ postgresql://
+    uri = os.environ.get('DATABASE_URL') or 'postgresql://mahjoub_online_1_db_user:S7dxtVGcKwrsM1QEzGOuPPcRL8dKxgXk@dpg-d79tuthr0fns73epej4g-a.oregon-postgres.render.com/mahjoub_online_1_db'
     
+    if uri and uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+        
+    SQLALCHEMY_DATABASE_URI = uri
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'MAHJOUB_SECRET_KEY_2026'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'Ali_Mahjoub_High_Energy_2026'
