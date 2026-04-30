207f35ed36d2711c1e5fb70acc34762587f836de
alimohm <69640611+alimohm@users.noreply.github.com>
2026-04-30 23:19:10 +0300

    Update _init__.py

--- admin_panel/_init__.py
+++ admin_panel/_init__.py
@@ -1,8 +1,12 @@
 from flask import Blueprint
 
-# قمنا بتغيير اسم المتغير البرمجي إلى admin_blueprint 
-# لكي لا يختلط الأمر على بايثون بين اسم "المجلد" واسم "الكائن"
-admin_blueprint = Blueprint('admin_panel', __name__, template_folder='templates')
+# قمنا بتغيير اسم الكائن إلى admin_blueprint لتمييزه عن اسم المجلد
+# وتحديد مسار القوالب ليكون المجلد templates داخل admin_panel
+admin_blueprint = Blueprint(
+    'admin_panel', 
+    __name__, 
+    template_folder='templates'
+)
 
-# استدعاء المسارات لربطها بالبلوبرنت المسمى حديثاً
+# استيراد المسارات لربطها بالبلوبرنت
 from . import routes
