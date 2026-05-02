0e32d8e48c6c44a461fee08d74626bc7b1e5f229
alimohm <69640611+alimohm@users.noreply.github.com>
2026-05-02 13:00:32 +0300

    Update user.py

--- core/models/user.py
+++ core/models/user.py
@@ -1,17 +1,18 @@
-from core import db
+from core import db, login_manager
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model, UserMixin):
-    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    password_hash = db.Column(db.String(200), nullable=False)
-    role = db.Column(db.String(20), default='admin')
-    is_active_account = db.Column(db.Boolean, default=True)
+    password_hash = db.Column(db.String(120), nullable=False)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
