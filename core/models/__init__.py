d19407e9e9888869aefcef24c940ed95f03671c8
alimohm <69640611+alimohm@users.noreply.github.com>
2026-05-02 14:07:05 +0300

    Update __init__.py

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,18 +1,12 @@
 # core/models/__init__.py
-
-# استيراد كائن db من الحزمة المركزية لضمان استخدام نفس المحرك
 from core import db
 
-# استيراد الموديلات بشكل مباشر ليتم التعرف عليها عند تشغيل db.create_all()
+# استيراد الموديل لضمان تسجيله في SQL Alchemy
 from core.models.user import User
 
-# ملاحظة: بمجرد إنشاء ملفات الموردين والطلبات، قم بإلغاء التعليق عن الأسطر التالية:
+# بمجرد أن تجهز ملفات الموردين والطلبات، قم بإلغاء التعليق:
 # from core.models.supplier import Supplier
 # from core.models.order import Order
 
-# تجميع الموديلات في قائمة واحدة لتسهيل العمليات البرمجية إذا لزم الأمر
-__all__ = [
-    'User',
-    # 'Supplier',
-    # 'Order'
-]
+# تعريف الحزم المصدرة
+__all__ = ['User']
