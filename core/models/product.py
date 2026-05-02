e4c8c460a2af98d126502927e4efd4f5bd1dda3e
alimohm <69640611+alimohm@users.noreply.github.com>
2026-05-02 12:37:55 +0300

    Update product.py

--- core/models/product.py
+++ core/models/product.py
@@ -3,17 +3,8 @@ from datetime import datetime
 
 class Product(db.Model):
     __tablename__ = 'products'
-
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200), nullable=False)
     price = db.Column(db.Float, nullable=False)
-    currency = db.Column(db.String(10), default='SAR')
-    category = db.Column(db.String(100), nullable=False)
-    
-    # القفل والمفتاح: الربط بجدول الموردين لإنهاء الخطأ
     supplier_id = db.Column(db.Integer, db.ForeignKey('suppliers.id'), nullable=False)
-    
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
-
-    def __repr__(self):
-        return f'<Product {self.name}>'
