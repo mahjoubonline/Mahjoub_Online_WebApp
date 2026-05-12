7de3dcf452334943efbbedd88f68bc507de718a3
alimohm <69640611+alimohm@users.noreply.github.com>
2026-05-12 13:12:28 +0300

    Delete core directory

--- core/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-    if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
-        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
-    
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'MAHJOUB_SECRET_2026')
