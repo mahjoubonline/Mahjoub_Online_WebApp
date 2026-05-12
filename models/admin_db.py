3cb3a65681e5d01a89ccb4bece06e0cedba93bf8
alimohm <69640611+alimohm@users.noreply.github.com>
2026-05-12 15:37:05 +0300

    Add AdminUser model for database integration

--- models/admin_db.py
+++ models/admin_db.py
@@ -1,1 +1,17 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
+db = SQLAlchemy()
+
+class AdminUser(db.Model):
+    __tablename__ = 'admin_users'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False) # لتخزين كلمة السر مشفرة
+    full_name = db.Column(db.String(100))
+    role = db.Column(db.String(20), default='founder') # دور "المؤسس"
+    last_login = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f'<AdminUser {self.username}>'
