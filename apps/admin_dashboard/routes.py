86c289f6a7231118d7b38ad539d16d5748d1da2e
alimohm <69640611+alimohm@users.noreply.github.com>
2026-05-12 14:36:07 +0300

    Update routes.py

--- apps/admin_dashboard/routes.py
+++ apps/admin_dashboard/routes.py
@@ -1,1 +1,14 @@
 
+from flask import Blueprint, render_template
+
+# نحدد هنا اسم المجلد templates لكي يعرف Flask أين يبحث
+admin_bp = Blueprint('admin', __name__, template_folder='templates')
+
+@admin_bp.route('/dashboard')
+def dashboard():
+    # استدعاء الملف من داخل مجلد admin الفرعي
+    return render_template('admin/dashboard_content.html')
+
+@admin_bp.route('/suppliers/manage')
+def manage_suppliers():
+    return render_template('admin/dashboard_content.html', title="الموردون المعتمدون")
