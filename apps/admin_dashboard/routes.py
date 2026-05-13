20e583eb74b39c85065764e992e401719ce0d1da
alimohm <69640611+alimohm@users.noreply.github.com>
2026-05-13 17:44:47 +0300

    Update routes.py

--- apps/admin_dashboard/routes.py
+++ apps/admin_dashboard/routes.py
@@ -1,9 +1,9 @@
 from flask import Blueprint, render_template
 
+# تأكد من أن الاسم هنا مطابق لما نستورده في run.py
 admin_dashboard = Blueprint('admin_dashboard', __name__, template_folder='templates')
 
 @admin_dashboard.route('/')
 @admin_dashboard.route('/dashboard')
 def dashboard():
-    # هنا نستدعي ملف المحتوى الذي سيرث الهيكل تلقائياً
     return render_template('admin/dashboard_content.html')
