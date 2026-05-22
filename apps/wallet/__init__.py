755a07d51cf49a14735954eb89d9bf341c641dee
Ali Mahjoub <alyasserandsonsfoodstuffs@gmail.com>
2026-05-22 18:24:05 +0300

    Update __init__.py

--- apps/wallet/__init__.py
+++ apps/wallet/__init__.py
@@ -1,5 +1,9 @@
+# coding: utf-8
 from flask import Blueprint
 
-wallet_bp = Blueprint('wallet', __name__)
+# تعريف الـ Blueprint باسم 'wallet' ليتطابق مع ما تم تسجيله في apps/__init__.py
+# وهذا هو الاسم الذي سيعتمد عليه url_for في القوالب
+wallet_bp = Blueprint('wallet', __name__, template_folder='templates')
 
+# استيراد الـ routes لضمان تسجيل المسارات داخل الـ Blueprint
 from . import routes
