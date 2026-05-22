3388d3744b817ff8f512c7f7a5623459d48818e7
Ali Mahjoub <alyasserandsonsfoodstuffs@gmail.com>
2026-05-22 16:33:21 +0300

    Update __init__.py

--- apps/auth_portal/__init__.py
+++ apps/auth_portal/__init__.py
@@ -1,5 +1,7 @@
+# apps/auth_portal/__init__.py
 from flask import Blueprint
 
-auth_portal_bp = Blueprint('auth_portal', __name__, template_folder='templates')
+# يجب أن يكون الاسم 'auth_blueprint' ليطابق ما استوردته في routes.py
+auth_blueprint = Blueprint('auth_portal', __name__, template_folder='templates')
 
 from . import routes
