3039d5ac315713f443a132c8c23431b485cadc9c
Ali Mahjoub <alyasserandsonsfoodstuffs@gmail.com>
2026-05-24 15:14:17 +0300

    Update __init__.py

--- apps/wallet/__init__.py
+++ apps/wallet/__init__.py
@@ -1,5 +1,12 @@
+# coding: utf-8
 from flask import Blueprint
 
-wallet_bp = Blueprint('wallet', __name__, template_folder='templates')
+# تأسيس المخطط المالي السيادي للمحافظ والتسويات
+wallet_blueprint = Blueprint(
+    'wallet',
+    __name__,
+    template_folder='templates',
+    static_folder='static'
+)
 
-from . import routes
+from apps.wallet import routes
