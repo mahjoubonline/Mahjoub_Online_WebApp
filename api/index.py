fca1da7312330cc12335fe1085164d3e290ebebc
alimohm <69640611+alimohm@users.noreply.github.com>
2026-06-01 22:20:14 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,9 +1,1 @@
-import os
-import sys
-
-# إضافة المجلد الرئيسي إلى مسار بايثون ليتمكن من استيراد apps
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
 from run import app
-
-# Vercel يحتاج لمتغير باسم app
