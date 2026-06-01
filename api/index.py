e02490aebb92a85a8da50dec68133b2225937f6d
alimohm <69640611+alimohm@users.noreply.github.com>
2026-06-01 19:42:27 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,10 +1,1 @@
-import sys
-import os
-
-# إضافة المجلد الرئيسي للمشروع لمسار النظام ليتمكن من رؤية مجلد 'apps'
-sys.path.append(os.getcwd())
-
-from apps import create_app
-
-# Vercel يحتاج لمتغير يسمى 'app' ليقوم بتشغيله
-app = create_app()
+from run import app
