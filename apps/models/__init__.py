2b310d41a0c741b538cf8955a54fc9170d3ae924
alimohm <69640611+alimohm@users.noreply.github.com>
2026-06-02 13:43:20 +0300

    Update __init__.py

--- apps/models/__init__.py
+++ apps/models/__init__.py
@@ -1,16 +1,14 @@
 # coding: utf-8
 # 📂 apps/models/__init__.py
-# تجميع وإشهار النماذج لسهولة الاستيراد من خارج حزمة models
+# حزمة الإشهار المركزية لنماذج قاعدة البيانات - منصة محجوب أونلاين 2026
 
 from apps.models.admin_db import AdminUser
 from apps.models.supplier_db import Supplier
 from apps.models.wallet_db import SupplierWallet as Wallet, WalletTransaction
 from apps.models.settlements_db import AdminSettlement
-
-# نستخدم الاستيراد المؤجل (Lazy Import) داخل ملفات الـ routes إذا لزم الأمر، 
-# لكن سنبقي الإشهار هنا بشرط أن لا يستورد statement_db أي موديل من هذا الملف.
 from apps.models.statement_db import SupplierStatement
 
+# القائمة السيادية الحاكمة للاستيراد الخارجي النظيف لمنع تداخل الحزم
 __all__ = [
     'AdminUser',
     'Supplier',
