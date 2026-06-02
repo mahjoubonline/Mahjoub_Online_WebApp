6d1a155053a556674f50cd166c420ba8e435c365
alimohm <69640611+alimohm@users.noreply.github.com>
2026-06-02 20:50:58 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
-# run.py
+# run.py المحسّن للإنتاج
 import os
 from apps import create_app
 
-# إنشاء التطبيق باستخدام المصنع (Factory Pattern)
 app = create_app()
 
 if __name__ == "__main__":
-    # تشغيل التطبيق (في الإنتاج Gunicorn يتجاهل هذا الجزء)
-    app.run()
+    # هذا الجزء سيعمل فقط عند تشغيله محلياً على جهازك
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
