235c95b3a8f390446dbbf2c5bcabf60498033041
alimohm <69640611+alimohm@users.noreply.github.com>
2026-06-03 00:05:11 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,12 +1,3 @@
-# coding: utf-8
-# 🚀 ملف إقلاع التطبيق الرسمي - منصة محجوب أونلاين
-
+# run.py
 from apps import create_app
-
-# إنشاء التطبيق باستخدام الـ Factory Pattern
-app = create_app()
-
-if __name__ == "__main__":
-    # هذا السطر يعمل فقط عند التشغيل المحلي على جهازك
-    # Render يتجاهل هذا الجزء ويستخدم Gunicorn مباشرة
-    app.run(host="0.0.0.0", port=5000, debug=True)
+app = create_app() # هنا تبدأ الرحلة بالكامل تحت إشراف المصنع!
