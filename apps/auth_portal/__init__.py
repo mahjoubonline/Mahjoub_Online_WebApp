4dd52b94a6441c1aa8b1d8887cec2d2d0a10517f
alimohm <69640611+alimohm@users.noreply.github.com>
2026-06-03 21:29:07 +0300

    Update __init__.py

--- apps/auth_portal/__init__.py
+++ apps/auth_portal/__init__.py
@@ -1,3 +1,8 @@
+# 📂 apps/auth_portal/__init__.py
 from flask import Blueprint
+
+# تعريف الـ Blueprint
 auth_portal = Blueprint('auth_portal', __name__, template_folder='templates')
+
+# استيراد المسارات هنا لتسجيلها في الـ Blueprint
 from . import routes
