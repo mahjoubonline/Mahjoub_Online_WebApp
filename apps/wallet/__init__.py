a36fa4f394697cf7f6ca3fffe4a2a98d9726eaa0
alimohm <69640611+alimohm@users.noreply.github.com>
2026-06-07 18:50:44 +0300

    Update __init__.py

--- apps/wallet/__init__.py
+++ apps/wallet/__init__.py
@@ -1,12 +1,1 @@
-# 📂 apps/wallet/__init__.py
-from flask import Blueprint
 
-# تعريف الـ Blueprint فقط
-wallet_app = Blueprint(
-    'wallet_app', 
-    __name__, 
-    template_folder='templates'
-)
-
-# ملاحظة هامة جداً: لا تقم باستيراد routes هنا! 
-# تسجيل المسارات سيتم عبر الـ Blueprint الذي قمت بتسجيله في apps/__init__.py الرئيسي.
