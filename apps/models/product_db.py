00cc10b40c3bf87094619be7288daa2116fcd2e3
علي محمد محمد علي محجوب <alimahgoob848@gmail.com>
2026-06-17 03:00:05 +0900

    Create product_db.py

---
+++ apps/models/product_db.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+# 📂 apps/models/product_db.py
+
+from apps.extensions import db
+from datetime import datetime
+
+class Product(db.Model):
+    __tablename__ = 'products'
+    
+    id = db.Column(db.String(100), primary_key=True) # معرف المنتج في قمرة
+    title = db.Column(db.String(255), nullable=False)
+    price = db.Column(db.Float, default=0.0)
+    tag = db.Column(db.String(100), index=True) # الوسم للفلترة (اسم شريك النجاح)
+    image_url = db.Column(db.String(500))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f'<Product {self.title}>'
