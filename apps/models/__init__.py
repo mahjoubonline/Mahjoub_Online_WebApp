6858c4bd9b923c2f72a8497ab9cbf54f3113ccfe
علي محمد محمد علي محجوب <alimahgoob848@gmail.com>
2026-06-17 04:11:50 +0900

    Update __init__.py

--- apps/models/__init__.py
+++ apps/models/__init__.py
@@ -2,21 +2,20 @@
 # 📂 apps/models/__init__.py
 
 from .admin_db import AdminUser
-from .supplier_db import Supplier
-from .wallet_db import SupplierWallet, WalletTransaction
 from .financial_db import FinancialLog, ExchangeRate
+from .supplier_db import Supplier
 from .vault_db import AdminVault, VaultTransaction
-from .bridge_db import Product, ProductVariant
+from .wallet_db import SupplierWallet, WalletTransaction
+
+# تم استبعاد bridge_db و settlements_db لأنهما غير موجودين في مجلدك الحالي
 
 __all__ = [
     'AdminUser',
-    'Supplier',
-    'SupplierWallet',
-    'WalletTransaction',
     'FinancialLog',
     'ExchangeRate',
+    'Supplier',
     'AdminVault',
     'VaultTransaction',
-    'Product',
-    'ProductVariant'
+    'SupplierWallet',
+    'WalletTransaction'
 ]
