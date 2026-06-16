b56eacef9cb7f92c1d9d78a7125319f7ad868ab1
علي محمد محمد علي محجوب <alimahgoob848@gmail.com>
2026-06-17 03:52:55 +0900

    Update __init__.py

--- apps/models/__init__.py
+++ apps/models/__init__.py
@@ -1,10 +1,9 @@
 # coding: utf-8
-# 📂 apps/models/__init__.py - النسخة بعد حذف Settlement
+# 📂 apps/models/__init__.py
 
 from .admin_db import AdminUser
 from .bridge_db import Product, ProductVariant
 from .financial_db import FinancialLog, ExchangeRate
-# سطر Settlement تم حذفه
 from .supplier_db import Supplier
 from .vault_db import AdminVault, VaultTransaction
 from .wallet_db import SupplierWallet, WalletTransaction
@@ -15,7 +14,6 @@ __all__ = [
     'ProductVariant',
     'FinancialLog',
     'ExchangeRate',
-    # 'Settlement' تم حذفه
     'Supplier',
     'AdminVault',
     'VaultTransaction',
