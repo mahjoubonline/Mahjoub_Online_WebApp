612681280c5ad6ddee137bcb235317923567e2c7
علي محمد محمد علي محجوب <alimahgoob848@gmail.com>
2026-06-17 22:05:51 +0300

    Update __init__.py

--- apps/orders/__init__.py
+++ apps/orders/__init__.py
@@ -1,8 +1,2 @@
-from flask import Blueprint, render_template, redirect, url_for
-
-main = Blueprint('main', __name__)
-
-@main.route('/')
-def index():
-    # توجيه المستخدم مباشرة إلى لوحة التحكم أو الصفحة المطلوبة
-    return redirect(url_for('orders.dashboard')) # تأكد أن هذا هو اسم المسار الصحيح للوحة التحكم
+# بدلاً من redirect(url_for('orders.dashboard'))
+return redirect(url_for('orders.orders_dashboard'))
