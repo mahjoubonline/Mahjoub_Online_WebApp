76b1af11e9342a3a5ca4ac13da50350afedfbc58
علي محمد محمد علي محجوب <alimahgoob848@gmail.com>
2026-06-17 16:51:25 +0300

    Update extensions.py

--- apps/extensions.py
+++ apps/extensions.py
@@ -1,8 +1,8 @@
 # 📂 apps/extensions.py
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 db = SQLAlchemy()
-login_manager = LoginManager()
 migrate = Migrate()
+login_manager = LoginManager()
