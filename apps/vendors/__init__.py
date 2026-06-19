bd16c20c2107357102f3fa43a6b1fef2ddf5d757
علي محمد محمد علي محجوب <alimahgoob848@gmail.com>
2026-06-20 00:13:13 +0300

    Update __init__.py

--- apps/vendors/__init__.py
+++ apps/vendors/__init__.py
@@ -1,11 +1,15 @@
-# apps/vendors/__init__.py
+# 📂 apps/vendors/__init__.py
 from flask import Blueprint
 
 # تعريف الـ Blueprint الخاص بالموردين
-# name: 'vendors' (اسم الـ Blueprint)
-# import_name: __name__ (يخبر Flask بموقع هذا التطبيق)
-# template_folder: 'templates' (المكان الذي سيجلب منه ملفات الـ HTML)
-vendors_bp = Blueprint('vendors', __name__, template_folder='templates')
+# مع تحديد المجلدات الخاصة بالقوالب والملفات الثابتة لضمان الاستقلالية
+vendors_bp = Blueprint(
+    'vendors', 
+    __name__, 
+    template_folder='templates', 
+    static_folder='static',
+    static_url_path='/vendors/static'
+)
 
 # استيراد المسارات لربطها بالـ Blueprint
 # نضعها في الأسفل لتجنب مشكلة "الاستيراد الدائري" (Circular Import)
