029fc851935e19c685f6d113fd80aa1aed4b3897
علي محمد محمد علي محجوب <alimahgoob848@gmail.com>
2026-06-19 23:45:06 +0300

    Update registry.py

--- apps/vendors/registry.py
+++ apps/vendors/registry.py
@@ -1,5 +1,5 @@
-# داخل apps/vendors/registry.py
-from .routes import vendors_bp # استيراد الـ Blueprint الخاص بك
+from .routes import vendors_bp # استيراد الـ Blueprint من ملف المسارات الخاص بك
 
 def register_app(app):
+    # هنا يتم تسجيل التطبيق تلقائياً عند تشغيل المصنع الأم
     app.register_blueprint(vendors_bp, url_prefix='/vendors')
