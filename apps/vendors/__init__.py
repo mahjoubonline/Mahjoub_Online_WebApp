5c7c96aaf54e410865bbe6b7318ef3a896cf2c77
علي محمد محمد علي محجوب <alimahgoob848@gmail.com>
2026-06-19 23:29:48 +0300

    Update __init__.py

--- apps/vendors/__init__.py
+++ apps/vendors/__init__.py
@@ -1,7 +1,8 @@
+# apps/vendors/__init__.py
 from flask import Blueprint
 
-# إنشاء الـ Blueprint الخاص بالموردين
-vendors_bp = Blueprint('vendors', __name__, template_folder='templates')
+# هذا هو "مدير القسم"
+vendors_manager = Blueprint('vendors_manager', __name__, url_prefix='/vendors')
 
-# استيراد الـ routes لكي يتم تسجيلها عند تشغيل التطبيق
-from . import routes
+# هنا يتم تجميع كل موديلات وقوالب ومسارات المورد داخل هذا القسم فقط
+from . import routes, models
