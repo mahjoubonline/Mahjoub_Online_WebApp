4d951cddcb6845a3b3dd0a1fe31a942f9392fa2c
علي محمد محمد علي محجوب <alimahgoob848@gmail.com>
2026-06-19 23:35:10 +0300

    Update __init__.py

--- apps/vendors/__init__.py
+++ apps/vendors/__init__.py
@@ -1,17 +1,7 @@
-from flask import Flask
+from flask import Blueprint
 
-def create_app():
-    app = Flask(__name__)
+# هذا هو "المدير" المسؤول عن قسم الموردين
+vendors_manager = Blueprint('vendors', __name__, template_folder='templates')
 
-    # المبدأ: لا نكتب أكواد، فقط نستدعي "المدراء"
-    from apps.admin_dashboard import admin_manager
-    from apps.vendors import vendors_manager
-    from apps.orders import orders_manager
-    # يمكنك إضافة أي عدد من المصانع هنا لاحقاً دون أي ضغط
-    
-    # التسجيل:
-    app.register_blueprint(admin_manager)
-    app.register_blueprint(vendors_manager)
-    app.register_blueprint(orders_manager)
-
-    return app
+# استدعاء المسارات (التي تحتوي على logic الموردين)
+from . import routes
