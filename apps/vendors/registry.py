b6c30bb47f5b67d81381077cb90ef2915e7b84c2
علي محمد محمد علي محجوب <alimahgoob848@gmail.com>
2026-06-20 19:23:54 +0300

    Update registry.py

--- apps/vendors/registry.py
+++ apps/vendors/registry.py
@@ -1,5 +1,9 @@
 # 📂 apps/vendors/registry.py
 from apps.vendors.routes import vendors_bp
 
-def register_app(app):  # <-- قمنا بتغيير الاسم هنا ليتطابق مع المصنع
+def register_app(app):
+    """
+    هذه الدالة تستدعى تلقائياً من قبل المصنع الرئيسي (apps/__init__.py)
+    لأن المصنع يبحث عن اسم register_app تحديداً.
+    """
     app.register_blueprint(vendors_bp, url_prefix='/vendors')
