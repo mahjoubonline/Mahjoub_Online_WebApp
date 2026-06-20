86bfcb17062471ee294185a42b206ab0f6bf5bc2
علي محمد محمد علي محجوب <alimahgoob848@gmail.com>
2026-06-20 19:23:14 +0300

    Update registry.py

--- apps/vendors/registry.py
+++ apps/vendors/registry.py
@@ -1,9 +1,5 @@
 # 📂 apps/vendors/registry.py
 from apps.vendors.routes import vendors_bp
 
-def register_vendors_module(app):
-    """
-    هذه الدالة تُستدعى تلقائياً من قبل محرك التطبيق الديناميكي
-    لربط مسارات الموردين بالتطبيق الأساسي.
-    """
+def register_app(app):  # <-- قمنا بتغيير الاسم هنا ليتطابق مع المصنع
     app.register_blueprint(vendors_bp, url_prefix='/vendors')
