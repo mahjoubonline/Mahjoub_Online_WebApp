dd6bd917256f719d2daa6ecdda4d3e60f9fccd96
علي محمد محمد علي محجوب <alimahgoob848@gmail.com>
2026-06-23 04:14:15 +0900

    Update __init__.py

--- apps/models/__init__.py
+++ apps/models/__init__.py
@@ -1,22 +1,19 @@
 # 📂 apps/models/__init__.py
-
 from .admin_db import AdminUser
 from .admin_staff_db import AdminStaff
 from .financials_db import OrderFinancial 
 from .marketers_db import Marketer
-
-# تم تصحيح الاسم هنا من ProcessedOrder إلى Order
-from .orders_db import Order 
-
+from .orders_db import Order
 from .otp_db import OTPVerification
 from .supplier_db import Supplier
 from .supplier_profile_db import SupplierProfile
 from .supplier_staff_db import SupplierStaff
 from .sync_log import SyncLog
-from .wallet_db import SupplierWallet
+# تم تعديل الاسم هنا ليطابق الكلاس VendorWallet
+from .wallet_db import VendorWallet 
 
 __all__ = [
     'AdminUser', 'AdminStaff', 'OrderFinancial', 'Marketer', 
     'Order', 'OTPVerification', 'Supplier', 
-    'SupplierProfile', 'SupplierStaff', 'SyncLog', 'SupplierWallet'
+    'SupplierProfile', 'SupplierStaff', 'SyncLog', 'VendorWallet'
 ]
