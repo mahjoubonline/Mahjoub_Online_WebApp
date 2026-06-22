32eaeb0497e85ae8bab252c6be139ad0247df9c3
علي محمد محمد علي محجوب <alimahgoob848@gmail.com>
2026-06-23 03:47:25 +0900

    Create admin_db.py

---
+++ apps/models/admin_db.py
@@ -0,0 +1,18 @@
+# 📂 apps/models/admin_db.py
+from apps.extensions import db
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class AdminUser(db.Model, UserMixin):
+    __tablename__ = 'admin_user'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+    role = db.Column(db.String(20), default='Owner')
+    phone_number = db.Column(db.String(20))
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
