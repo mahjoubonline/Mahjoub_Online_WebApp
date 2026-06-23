986cbc93db3975343d21f4ed85e0f00d022356ae
علي محمد محمد علي محجوب <alimahgoob848@gmail.com>
2026-06-24 02:01:31 +0900

    Update __init__.py

--- apps/admin_dashboard/__init__.py
+++ apps/admin_dashboard/__init__.py
@@ -1,14 +1,2 @@
-# coding: utf-8
 # 📂 apps/admin_dashboard/__init__.py
-
-from flask import Blueprint
-
-# تعريف الـ Blueprint الخاص بلوحة التحكم
-admin_dashboard = Blueprint(
-    'admin_dashboard', 
-    __name__, 
-    template_folder='templates'  # المجلد الذي يحتوي على ملفات HTML الخاصة بالداشبورد
-)
-
-# استيراد المسارات لربطها بالـ Blueprint
-from . import routes
+from .routes import admin_dashboard
