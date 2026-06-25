e58282e17849b7d20bbb87d564548c12a33ea34d
علي محمد محمد علي محجوب <alimahgoob848@gmail.com>
2026-06-26 02:49:52 +0900

    Update __init__.py

--- apps/suppliers_dashboard/__init__.py
+++ apps/suppliers_dashboard/__init__.py
@@ -1,5 +1,1 @@
-# 📂 apps/suppliers_dashboard/__init__.py
-
-# هذا الملف يجعل المجلد حزمة بايثون (Package)
-# يمكنك تركه فارغاً، أو استيراد الـ Blueprint هنا لتسهيل الوصول إليه
-from apps.suppliers_dashboard.routes import dashboard_bp
+# اترك هذا الملف فارغاً، يكفي وجوده ليعرف النظام أن المجلد حزمة Python
