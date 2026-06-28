0728c0eba48f4d24b3dbfb2a3d4c969bf4fabebc
علي محمد محمد علي محجوب <alimahgoob848@gmail.com>
2026-06-28 17:54:44 +0300

    Update registry.py

--- apps/orders/registry.py
+++ apps/orders/registry.py
@@ -1,5 +1,12 @@
+# coding: utf-8
 # 📂 apps/orders/registry.py
+
 from apps.orders.routes import orders_bp
 
 def register_module(app):
+    """
+    هذه الدالة هي التي يبحث عنها ملف apps/__init__.py 
+    للقيام بالتسجيل التلقائي لموديول الطلبات.
+    """
     app.register_blueprint(orders_bp, url_prefix='/orders')
+    print("✅ [Registry]: تم تسجيل موديول 'Orders' بنجاح.")
