981896b3020d9404c500b179b3b282409a3bad14
علي محمد محمد علي محجوب <alimahgoob848@gmail.com>
2026-06-29 21:15:32 +0300

    Update registry.py

--- apps/admin_financial_management/registry.py
+++ apps/admin_financial_management/registry.py
@@ -1,1 +1,12 @@
+# coding: utf-8
+# 📂 apps/admin_financial_management/registry.py
 
+from apps.admin_financial_management.routes import admin_financial_bp
+
+def register_module(app):
+    """
+    تسجيل موديول الإدارة المالية للطلبات.
+    يتم تسجيله على المسار /admin/financial
+    """
+    app.register_blueprint(admin_financial_bp, url_prefix='/admin/financial')
+    print("✅ [Registry]: تم تسجيل موديول 'Admin Financial Management' بنجاح على المسار /admin/financial")
