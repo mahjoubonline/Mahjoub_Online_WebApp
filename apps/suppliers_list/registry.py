1049a3a936505737bdede1a970039a5a55d3d261
علي محمد محمد علي محجوب <alimahgoob848@gmail.com>
2026-06-29 19:16:01 +0300

    Update registry.py

--- apps/suppliers_list/registry.py
+++ apps/suppliers_list/registry.py
@@ -1,1 +1,12 @@
+# coding: utf-8
+# 📂 apps/suppliers_list/registry.py
 
+from apps.suppliers_list.routes import supplier_bp
+
+def register_module(app):
+    """
+    هذه الدالة هي التي يبحث عنها ملف apps/__init__.py 
+    للقيام بالتسجيل التلقائي لموديول قائمة الشركاء (الموردين).
+    """
+    app.register_blueprint(supplier_bp, url_prefix='/suppliers')
+    print("✅ [Registry]: تم تسجيل موديول 'Suppliers' بنجاح.")
