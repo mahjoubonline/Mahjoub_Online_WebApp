b8a8ccc5ec7dcde8a4b4eb84691be1bfee1f67ab
Jui 88 <jui586053@gmail.com>
2026-06-30 19:31:26 +0300

    Update registry.py

--- apps/admin_platform_treasury/registry.py
+++ apps/admin_platform_treasury/registry.py
@@ -1,3 +1,6 @@
+# coding: utf-8
 from .routes import treasury_bp
+
 def register_module(app):
     app.register_blueprint(treasury_bp, url_prefix='/admin/treasury')
+    print("✅ [Registry]: تم تسجيل موديول 'admin_platform_treasury' بنجاح.")
