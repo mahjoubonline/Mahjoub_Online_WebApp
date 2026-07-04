5c3f4e8946a79d39aa544f2149f26b68f729c6e3
Jui 88 <jui586053@gmail.com>
2026-07-04 19:21:07 +0300

    Update registry.py

--- apps/admin_suppliers_add/registry.py
+++ apps/admin_suppliers_add/registry.py
@@ -1,19 +1,18 @@
 # coding: utf-8
-# 📂 apps/admin_suppliers_add/registry.py
+from .routes import suppliers_bp
 
-from .routes import admin_suppliers_add_bp # تأكد من اسم الـ Blueprint لديك
+MODULE_NAME = "إدارة الشركاء"
+MODULE_ICON = "fa-users"
 
-MODULE_NAME = "إدارة الموردين"
-MODULE_ICON = "fa-user-plus"
-
-# تم تسطيح الروابط هنا أيضاً
+# هنا نجمع كل الروابط الخاصة بالموردين
 LINKS = {
+    "قائمة الشركاء": "suppliers_bp.list_suppliers",
     "تعميد شريك": "admin_suppliers_add_bp.add_supplier_or_staff"
 }
 
 def register_module(app):
     try:
-        app.register_blueprint(admin_suppliers_add_bp, url_prefix='/admin/suppliers_add')
-        print("✅ [Registry]: تم تسجيل موديول 'admin_suppliers_add' بنجاح.")
+        app.register_blueprint(suppliers_bp, url_prefix='/admin/suppliers')
+        print("✅ [Registry]: تم تسجيل موديول 'admin_suppliers_list' بنجاح.")
     except Exception as e:
-        print(f"❌ [Registry Error]: فشل تسجيل موديول 'admin_suppliers_add': {e}")
+        print(f"❌ [Registry Error]: فشل تسجيل موديول 'admin_suppliers_list': {e}")
