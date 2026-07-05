6c8a9c096b45dec9111f469df48dfd0fb5095558
Jui 88 <jui586053@gmail.com>
2026-07-05 20:11:04 +0300

    Update registry.py

--- apps/admin_financial_control/registry.py
+++ apps/admin_financial_control/registry.py
@@ -1,10 +1,9 @@
 # 📂 apps/admin_financial_control/registry.py
-
 MODULE_NAME = "الرقابة المالية"
 MODULE_ICON = "fas fa-shield-alt"
 
 LINKS = {
     "الخزينة المركزية": "treasury_bp.dashboard",
     "محفظة الموردين": "wallet_app.dashboard",
-    "أسعار الصرف": "admin_exchange.manage_rates"  # إضافة الرابط هنا
+    "أسعار الصرف": "admin_exchange.manage_rates"
 }
