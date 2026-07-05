6f43d260329f2c21f8adb39863c861e09fb83f9f
Jui 88 <jui586053@gmail.com>
2026-07-05 19:36:13 +0300

    Update routes.py

--- apps/admin_exchange/routes.py
+++ apps/admin_exchange/routes.py
@@ -1,1 +1,33 @@
+# 📂 apps/admin/exchange_routes.py
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+from flask_login import login_required, current_user
+from apps.extensions import db
+from apps.models.exchange_db import ExchangeRate
 
+admin_exchange_bp = Blueprint('admin_exchange', __name__, template_folder='templates')
+
+@admin_exchange_bp.route('/exchange-rates', methods=['GET', 'POST'])
+@login_required
+def manage_rates():
+    # تأكد من أنك تملك صلاحيات المدير
+    if current_user.rank != 'admin': 
+        return "غير مصرح لك", 403
+
+    if request.method == 'POST':
+        code = request.form.get('currency_code')
+        new_rate = request.form.get('rate')
+        
+        rate_entry = ExchangeRate.query.filter_by(currency_code=code).first()
+        if rate_entry:
+            rate_entry.rate_to_sar = new_rate
+            rate_entry.last_updated_by = current_user.username
+        else:
+            new_entry = ExchangeRate(currency_code=code, rate_to_sar=new_rate, last_updated_by=current_user.username)
+            db.session.add(new_entry)
+        
+        db.session.commit()
+        flash(f"تم تحديث سعر {code} بنجاح", "success")
+        return redirect(url_for('admin_exchange.manage_rates'))
+
+    rates = ExchangeRate.query.all()
+    return render_template('admin/exchange_rates.html', rates=rates)
