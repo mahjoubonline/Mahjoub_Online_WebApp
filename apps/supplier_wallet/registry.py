2e0b097d989d354f62dc5a45d8fe2491ac72258e
Jui 88 <jui586053@gmail.com>
2026-07-06 13:43:29 +0300

    Update registry.py

--- apps/supplier_wallet/registry.py
+++ apps/supplier_wallet/registry.py
@@ -1,12 +1,11 @@
 # coding: utf-8
-# 📂 apps/supplier_wallet/registry.py
+# 📂 apps/suppliers_orders/registry.py
 
-from apps.supplier_wallet.routes import supplier_wallet_bp
+from apps.suppliers_orders.routes import suppliers_orders_bp
 
-# يجب تغيير الاسم هنا إلى 'register_module' ليتعرف عليها المصنع تلقائياً
 def register_module(app):
     """
-    دالة موحدة لتسجيل الموديول، يتوقع المصنع هذا الاسم.
+    تسجيل موديول طلبات المورد تلقائياً عبر المصنع.
     """
-    app.register_blueprint(supplier_wallet_bp, url_prefix='/supplier/wallet')
-    print("✅ [Registry] تم تسجيل موديول 'محفظة المورد' بنجاح.")
+    app.register_blueprint(suppliers_orders_bp, url_prefix='/suppliers_orders')
+    print("✅ [Registry] تم تسجيل موديول 'طلبات المورد' بنجاح.")
