f00a957f990c565e196d1ca81a9f393c24648b53
Jui 88 <jui586053@gmail.com>
2026-07-06 21:26:08 +0300

    Update registry.py

--- apps/suppliers_dashboard/registry.py
+++ apps/suppliers_dashboard/registry.py
@@ -1,6 +1,5 @@
 # coding: utf-8
-from apps.suppliers_dashboard.routes import suppliers_dashboard_bp
+from apps.suppliers_dashboard.routes import suppliers_dashboard_bp # تأكد من تطابق الاسم
 
 def register_module(app):
-    # التسجيل ببادئة /supplier ليكون الرابط الكامل: /supplier/dashboard
     app.register_blueprint(suppliers_dashboard_bp, url_prefix='/supplier')
