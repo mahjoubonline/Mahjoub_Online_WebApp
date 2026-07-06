e63a30cd5c676e8627f69065ba1f6bddabac7031
Jui 88 <jui586053@gmail.com>
2026-07-06 21:14:21 +0300

    Update registry.py

--- apps/suppliers_dashboard/registry.py
+++ apps/suppliers_dashboard/registry.py
@@ -1,10 +1,6 @@
 # coding: utf-8
-from apps.suppliers_auth.routes import suppliers_auth_bp
+from apps.suppliers_dashboard.routes import suppliers_dashboard_bp
 
 def register_module(app):
-    try:
-        # تسجيل مسار الدخول والخروج
-        app.register_blueprint(suppliers_auth_bp, url_prefix='/supplier')
-        print("✅ [Registry]: تم تسجيل موديول 'suppliers_auth' بنجاح.")
-    except Exception as e:
-        print(f"❌ [Registry Error]: {e}")
+    # التسجيل ببادئة /supplier ليكون الرابط الكامل: /supplier/dashboard
+    app.register_blueprint(suppliers_dashboard_bp, url_prefix='/supplier')
