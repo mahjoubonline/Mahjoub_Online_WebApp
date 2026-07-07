e98764ece69b30b82d738e7e7cbccd26f3e253f8
Jui 88 <jui586053@gmail.com>
2026-07-07 18:17:19 +0300

    Update graphql_client.py

--- apps/services/graphql_client.py
+++ apps/services/graphql_client.py
@@ -1,1 +1,32 @@
-services/graphql_client.py
+import requests
+from config import Config
+
+class QomrahGraphQLClient:
+    @staticmethod
+    def get_order_details(order_id):
+        query = """
+        query GetOrder($id: ID!) {
+            order(id: $id) {
+                id
+                supplier_id
+                total_price
+                tracking_tag
+                currency
+                items {
+                    title
+                    qty
+                    subtotal
+                    sku
+                }
+            }
+        }
+        """
+        # إضافة المفتاح في الـ Headers
+        headers = {"Authorization": f"Bearer {Config.QUMRA_API_KEY}"}
+        # تنفيذ الطلب للرابط الذي حددته
+        response = requests.post(
+            Config.QUMRA_API_URL, 
+            json={'query': query, 'variables': {'id': order_id}},
+            headers=headers
+        )
+        return response.json().get('data', {}).get('order')
