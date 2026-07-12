fb1d2895bc37f5ed928bfcf2a1ebf30136eee862
Jui 88 <jui586053@gmail.com>
2026-07-12 14:32:40 +0300

    Update routes.py

--- apps/supplier_wallet/routes.py
+++ apps/supplier_wallet/routes.py
@@ -1,5 +1,5 @@
 # coding: utf-8
-# 📂 apps/suppliers_wallet/routes.py
+# 📂 apps/supplier_wallet/routes.py
 
 from flask import Blueprint, render_template, abort, request, session
 from flask_login import login_required, current_user
