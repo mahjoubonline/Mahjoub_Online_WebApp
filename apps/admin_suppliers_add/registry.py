a4d66ce3613ef71fd4fc4cb7fa02b54fd5725b2e
Jui 88 <jui586053@gmail.com>
2026-07-15 23:45:57 +0300

    Update registry.py

--- apps/admin_suppliers_add/registry.py
+++ apps/admin_suppliers_add/registry.py
@@ -1,7 +1,15 @@
 # 📂 apps/admin_suppliers_add/registry.py
+from .routes import admin_suppliers_add_bp
+
+# الإعدادات للظهور في القائمة الجانبية
+MODULE_NAME = "الموردين"
+MODULE_ICON = "fas fa-user-plus"
+LINKS = {
+    "admin_suppliers_add_bp.index": "إضافة مورد جديد"
+}
+
 def register_module(app):
     try:
-        from .routes import admin_suppliers_add_bp
         app.register_blueprint(admin_suppliers_add_bp, url_prefix='/admin/suppliers_add')
         print("✅ [Registry]: تم تسجيل موديول 'admin_suppliers_add' بنجاح.")
     except Exception as e:
